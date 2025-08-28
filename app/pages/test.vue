fb7f65a6dbf42268cf01b9447644a1677baac402
MOEN ERAK <raknosmoke000@gmail.com>
2025-08-28 15:38:44 +0700

    iDontKnow

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,5 +1,18 @@
 
 <template>
 
-    
+<section class="wrapper">
+
+</section>
+
 </template>
+
+<style scoped>
+section.wrapper {
+  width: 100dvw;
+  height: 100dvh;
+  background: linear-gradient(to right, rgb(95, 255, 95) 1px, transparent 0) , linear-gradient(to bottom, rgb(95, 255, 95) 1px, transparent 0) ;
+  background-size: 10dvw 10dvw;
+  background-position: center;
+}
+</style>
